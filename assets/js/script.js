68aa3463117ead910f3d8547f30a442202442a28
Grainne Donegan <grainnejdonegan@gmail.com>
2022-08-11 14:42:24 +0000

    update rough structure in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,19 @@
-// set up the website and listen out for the 1 or 2 player option button - remove afterwards
+// start - removes the button and moves down the guessing-letters section
 
-// For 1 player: only 5 letter words - get JSON of all possible options and randomly select one
-// For 2 player: prompt the user to enter a word (should I use prompt() or something else to fit aesthetic of page) - min 4, max 9 letters
+// only 7 letter words - get JSON of all possible options and randomly select one
 
 // set the number of underscores to the number of letters in the words
 
 // start counter for wrong letters - only 7 possible wrong attempts 
 
-// Let the user both click/select the button on the screen and press the key - can I just copy the keyboard from wordle and source it?
+// Let the user both click/select the button on the screen and press the key
 
 // if a letter is pressed and is wrong, the letter block is red, the counter is added and the hangman picture is updated
 
 /* if a letter is pressed and is right, the letter block turns green, the word checks if its equal to the original input
 and if not, it lets the user pick another letter */
 
-// if the counter reaches 7, send message (either new html page or can I put temp box on page) that lets the user play again
+// if the counter reaches 7, send message that lets the user play again
 
 // if the word is obtained, send message to say well done and play again
 
